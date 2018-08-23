2b130d3305223b2536da4562f6b0daca0edab047
bmpitzen <bmpitzen@icloud.com>
2018-08-23 09:40:18 -0400

    Form works to accept and display new albums.

--- record_collection/db/schema.rb
+++ record_collection/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_181419) do
+ActiveRecord::Schema.define(version: 2018_08_23_004949) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title"
+    t.string "artist_name"
+    t.string "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "collections", force: :cascade do |t|
     t.datetime "created_at", null: false
